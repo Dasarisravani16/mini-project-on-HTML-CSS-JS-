dc80270481f052c5f1421e7679183b5c88de7abc
Dasarisravani16 <dasarisravani209@gmail.com>
2025-06-22 14:08:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+
+    
+ function entered(){
+    c1=document.getElementById("c1");
+    c1.style.backgroundColor = ("rgb(111, 104, 130)");
+
+
+
+}
+function leaved(){
+    c1=document.getElementById("c1");
+    c1.style.backgroundColor = ("rgb(156, 152, 110)")
+}
+   
+function entered(){
+    c2=document.getElementById("c2");
+    c2.style.backgroundColor = ("rgb(111, 104, 130)");
+
+
+
+}
+function leaved(){
+    c2=document.getElementById("c2");
+    c2.style.backgroundColor = ("rgb(156, 152, 110)")
+}
+function entered(){
+    c3=document.getElementById("c3");
+    c3.style.backgroundColor = ("rgb(111, 104, 130)");
+
+
+
+}
+function leaved(){
+    c3=document.getElementById("c3");
+    c3.style.backgroundColor = ("rgb(156, 152, 110)")
+}
+   
+function entered(){
+    c4=document.getElementById("c4");
+    c4.style.backgroundColor = ("rgb(111, 104, 130)");
+
+
+
+}
+function leaved(){
+    c4=document.getElementById("c4");
+    c4.style.backgroundColor = ("rgb(156, 152, 110)")
+}
+
+
+   
+
+
+                                                                            
